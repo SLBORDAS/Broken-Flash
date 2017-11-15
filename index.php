ebb4562fe3942069e2e77ff571673e6894116130
SLBORDAS <matthieu.bordas@sfr.fr>
2017-11-15 15:45:38 +0100

    mise en ligne

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html class="responsive" lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./style.css">
+    <title>Broken Flash</title>
+    <link rel="shortcut icon" href="broken-flash-logo.png" type="image/x-icon">
+
+  </head>
+  <body>
+<div class="BIBP">
+  <p>Biblioteque public</p>
+</div>
+<div class="BIBPE">
+  <p>Biblioteque Personnel</p>
+</div>
+<div class="Comm">
+<p>Communauté</p>
+</div>
+<div class="supp">
+  <p>Support</p>
+</div>
+  </body>
+</html>
